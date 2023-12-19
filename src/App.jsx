319ef2891f69d986f87504c7bf475bbd7a2dc259
Jayati-Wajire <jaya15403@gmail.com>
2023-12-19 19:21:32 +0530

    Required changes added

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Jayati Wajire",
+  title: "Microsoft Learn Student Ambassador",
+  email: "jayati.wajire21@pccoepune.org",
+  gitHub: "Jayati-Wajire",
+  instagram: "@jayatiwajire",
+  linkedIn: "Jayati Wajire",
+  medium: "jjj",
+  twitter: "No twitter",
+  youTube: "Jayati Wajire",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mountain.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Himalayan Mountains";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Find me as a Microsoft Learn Student Ambassador!",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "You can now find me on the official website of Microsoft LSA.",
+    url: "https://mvp.microsoft.com/en-US/studentambassadors/profile/b3ef45b8-87e4-4d83-9b49-1f0a638e797e",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My Github Account",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "You can check my Projects on Github now.",
+    url: "https://github.com/Jayati-Wajire",
   },
   {
-    title: "My Resume Site",
+    title: "My LinkedIn",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "My technical achievements and updates are available here!",
+    url: "https://www.linkedin.com/in/jayati-wajire-27056423a/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Instagram Account",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "You can find my nontechnical and personal updates here on my instagram.",
+    url: "https://www.instagram.com/jayatiwajire/",
   },
 ];
 
